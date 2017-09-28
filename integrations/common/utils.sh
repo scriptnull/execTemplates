4085f2c44a81c50b52efc8efdd7d68e9c93a15b5
Vishnu Bharathi <vishnubharathi04@gmail.com>
2017-09-28 13:33:48 +0530

    adds documentation for utils functions

--- integrations/common/utils.sh
+++ integrations/common/utils.sh
@@ -1,15 +1,21 @@
 #!/bin/bash -e
 
+# is_empty checks if given value is "" or "null"
+# args: string
 is_empty() {
   [[ -z "$1" ]] || [[ "$1" == "null" ]]
 }
 
+# csv_has_value checks if a `value` is present in a `csv` comma separated value string
+# args: csv(string), value(string)
 csv_has_value() {
   local csv=$1
   local value=$2
   [[ $csv =~ (^|,)$value(,|$) ]]
 }
 
+# is_docker_email_deprecated checks if email argument is deprected in docker command for current docker version
+# args: none
 is_docker_email_deprecated() {
   local docker_version=""
   local docker_major_version=""
