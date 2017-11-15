58396b43be5b4b591aad7448a61313aa9a10db6c
Bharath Upadhya <1992bharath@gmail.com>
2017-11-15 13:15:19 +0530

    updates amazonKeys integration scripts

--- resources/cliConfig/amazonKeys/cleanup.sh
+++ resources/cliConfig/amazonKeys/cleanup.sh
@@ -1,21 +1,76 @@
-cleanup_amazon_keys() {
-  local aws_config_path=~/.aws
+#!/bin/bash -e
+
+readonly ROOT_DIR="$(dirname "$0")/../../.."
+readonly COMMON_DIR="$ROOT_DIR/resources/common"
+readonly HELPERS_PATH="$COMMON_DIR/_helpers.sh"
+readonly LOGGER_PATH="$COMMON_DIR/_logger.sh"
+
+# shellcheck source=integrations/common/_helpers.sh
+source "$HELPERS_PATH"
+# shellcheck source=integrations/common/_logger.sh
+source "$LOGGER_PATH"
+
+export RESOURCE_NAME=""
+export SCOPE=""
+
+help() {
+  echo "
+  Usage:
+    $SCRIPT_NAME <resource_name> [scope]
+  "
+}
+
+cleanup_scope_configure() {
+  _log_msg "Cleaning up scope configure"
+
+  local aws_config_path
+  aws_config_path=~/.aws
   if [ -d "$aws_config_path" ]; then
     rm -rf $aws_config_path
   fi
+
+  _log_success "Successfully cleaned up scope configure"
 }
 
-cleanup_amazon_keys_ecr() {
-  local docker_config_path=~/.docker
+cleanup_scope_ecr() {
+  _log_msg "Cleaning up scope ecr"
+
+  local docker_config_path
+  docker_config_path=~/.docker
   if [ -d "$docker_config_path" ]; then
     rm -rf $docker_config_path
   fi
+
+  _log_success "Successfully cleaned up scope ecr"
 }
 
-trap before_exit EXIT
-exec_grp "cleanup_amazon_keys" "Cleaning up configuration for <%= obj.dependency.name %>" "false"
+cleanup() {
+  RESOURCE_NAME=${ARGS[0]}
+  SCOPES=${ARGS[1]}
 
-<% if (obj.dependency.step && obj.dependency.step.scopes && _.contains(obj.dependency.step.scopes, 'ecr')) { %>
-  trap before_exit EXIT
-  exec_grp "cleanup_amazon_keys_ecr" "Cleaning up ECR configuration for <%= obj.dependency.name %>" "false"
-<% } %>
+  _log_grp "Cleaning up resource $RESOURCE_NAME"
+
+  cleanup_scope_configure
+  if _csv_has_value "$SCOPES" "ecr"; then
+    cleanup_scope_ecr
+  fi
+}
+
+main() {
+  if [[ "${#ARGS[@]}" -gt 0 ]]; then
+    case "${ARGS[0]}" in
+      --help)
+        help
+        exit 0
+        ;;
+      *)
+        cleanup
+        ;;
+    esac
+  else
+    help
+    exit 1
+  fi
+}
+
+main
