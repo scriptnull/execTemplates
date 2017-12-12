8170a7974205a59ac3e34747517725b9de3fc891
Vishnu Bharathi <vishnubharathi04@gmail.com>
2017-12-12 19:13:14 +0530

    escapes quotes correctly

--- WindowsServer_2016/job/header.ps1
+++ WindowsServer_2016/job/header.ps1
@@ -15,7 +15,7 @@ Function exec_grp([string]$group_name, [string]$group_message,[Bool]$is_shown = 
   # TODO: migrate to unix time if this does not work
   $group_start_timestamp = Get-Date -format "%s"
   Write-Output ""
-  Write-Output "__SH__GROUP__START__|{\"type\":\"grp\",\"sequenceNumber\":\"$group_start_timestamp\",\"id\":\"$group_uuid\",\"is_shown\":\"$is_shown\"}|$group_message"
+  Write-Output "__SH__GROUP__START__|{`"type`":`"grp`",`"sequenceNumber`":`"$group_start_timestamp`",`"id`":`"$group_uuid`",`"is_shown`":`"$is_shown`"}|$group_message"
 
   # TODO: export current_grp and current_grp_uuid
 
@@ -28,5 +28,5 @@ Function exec_grp([string]$group_name, [string]$group_message,[Bool]$is_shown = 
 
   # TODO: migrate to unix time if this does not work
   $group_end_timestamp = Get-Date -format "%s"
-  Write-Output "__SH__GROUP__END__|{\"type\":\"grp\",\"sequenceNumber\":\"$group_end_timestamp\",\"id\":\"$group_uuid\",\"is_shown\":\"$is_shown\",\"exitcode\":\"$group_status\"}|$group_message"
+  Write-Output "__SH__GROUP__END__|{`"type`":`"grp`",`"sequenceNumber`":`"$group_end_timestamp`",`"id`":`"$group_uuid`",`"is_shown`":`"$is_shown`",`"exitcode`":`"$group_status`"}|$group_message"
 }
