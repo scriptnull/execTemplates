9be083a73ebdd14264c79d08e488af528eafe050
Vishnu Bharathi <vishnubharathi04@gmail.com>
2017-12-15 18:42:39 +0530

    moves before exit

--- WindowsServer_2016/job/header.ps1
+++ WindowsServer_2016/job/header.ps1
@@ -1,16 +1,3 @@
-Function before_exit() {
-  if ($global:is_success) {
-    if (Get-Command "on_success" -errorAction SilentlyContinue)
-    {
-      exec_cmd "on_success"
-    }
-
-    Write-Output "__SH__SCRIPT_END_SUCCESS__";
-  } else {
-    Write-Output "__SH__SCRIPT_END_FAILURE__";
-  }
-}
-
 Function exec_cmd([string]$cmd) {
   $cmd_uuid = [guid]::NewGuid().Guid
   $cmd_start_timestamp = Get-Date -format "%s"
--- WindowsServer_2016/job/task.ps1
+++ WindowsServer_2016/job/task.ps1
@@ -11,6 +11,32 @@ Function on_success() {
 }
 <% } %>
 
+<% if (obj.onFailure) { %>
+Function on_failure() {
+  <% _.each(obj.onFailure.script, function(cmd) { %>
+    Invoke-Expression '<%= cmd %>'
+  <% }); %>
+}
+<% } %>
+
+Function before_exit() {
+  if ($global:is_success) {
+    if (Get-Command "on_success" -errorAction SilentlyContinue)
+    {
+      exec_cmd "on_success"
+    }
+
+    Write-Output "__SH__SCRIPT_END_SUCCESS__";
+  } else {
+    if (Get-Command "on_failure" -errorAction SilentlyContinue)
+    {
+      exec_cmd "on_failure"
+    }
+
+    Write-Output "__SH__SCRIPT_END_FAILURE__";
+  }
+}
+
 Function main() {
   $global:is_success = $TRUE
   Try
