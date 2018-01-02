3574e9fb741cb0f8acf75299f84646a076ffa39a
Ragesh Krishna <ragesh@rageshkrishna.com>
2018-01-02 11:45:40 +0530

    Fix replace_placeholders.ps1 to match the other templates

--- WindowsServer_2016/job/replace_placeholders.ps1
+++ WindowsServer_2016/job/replace_placeholders.ps1
@@ -1,5 +1,8 @@
 Function replace_placeholders() {
-    exec_cmd 'shippable_replace <%= cmd %>'
+    $versionPath = "<%= obj.versionPath %>"
+    if (Test-Path "$versionPath") {
+        exec_cmd "shippable_replace $versionPath"
+    }
 }
 
 replace_placeholders
