d650cddc4f9722a3b64abc764230a481d2af3687
Ragesh Krishna <ragesh@rageshkrishna.com>
2018-01-04 13:46:45 +0530

    Enable container builds on Windows

--- WindowsServer_2016/job/boot.ps1
+++ WindowsServer_2016/job/boot.ps1
@@ -1,1 +1,13 @@
-echo "TODO: Complete boot.ps1"
+function boot() {
+    if ($env:TASK_CONTAINER_IMAGE_SHOULD_PULL -eq $TRUE) {
+        exec_cmd "docker pull $env:TASK_CONTAINER_IMAGE"
+    }
+
+    exec_cmd "docker run $env:TASK_CONTAINER_OPTIONS $env:TASK_CONTAINER_IMAGE $env:TASK_CONTAINER_COMMAND"
+
+    $global:is_success = $TRUE
+    Write-Output "__SH__SCRIPT_END_SUCCESS__";
+}
+
+exec_grp "boot" "Booting up container for task: $env:TASK_NAME"
+
--- WindowsServer_2016/job/envs.ps1
+++ WindowsServer_2016/job/envs.ps1
@@ -32,3 +32,9 @@ $env:SHIPPABLE_NODE_ARCHITECTURE = "<%= obj.shippableRuntimeEnvs.shippableNodeAr
 $env:SHIPPABLE_NODE_OPERATING_SYSTEM = "<%= obj.shippableRuntimeEnvs.shippableNodeOperatingSystem %>"
 $env:TASK_NAME = "<%= obj.shippableRuntimeEnvs.taskName %>"
 $env:TASK_IN_CONTAINER = <%= obj.shippableRuntimeEnvs.isTaskInContainer ? "$TRUE" : "$FALSE" %>
+if ($env:TASK_IN_CONTAINER -eq $TRUE) {
+  $env:TASK_CONTAINER_OPTIONS = "<%= obj.shippableRuntimeEnvs.taskContainerOptions %>"
+  $env:TASK_CONTAINER_IMAGE = "<%= obj.shippableRuntimeEnvs.taskContainerImage %>"
+  $env:TASK_CONTAINER_IMAGE_SHOULD_PULL = <%= obj.shippableRuntimeEnvs.shouldPullTaskContainerImage ? "$TRUE": "$FALSE" %>
+  $env:TASK_CONTAINER_COMMAND = "<%= obj.shippableRuntimeEnvs.taskContainerCommand %>"
+}
