e309f40c1b9037f1c420ca421a0d6b69b73bf9a7
Bharath Upadhya <1992bharath@gmail.com>
2018-01-08 18:28:02 +0530

    updates docker boot logic to add docker wait

--- Ubuntu_14.04/job/boot.sh
+++ Ubuntu_14.04/job/boot.sh
@@ -10,7 +10,19 @@ boot() {
     exec_cmd "sudo docker pull $TASK_CONTAINER_IMAGE"
   fi
 
-  exec_cmd "sudo docker run $TASK_CONTAINER_OPTIONS $TASK_CONTAINER_IMAGE $TASK_CONTAINER_COMMAND"
+  exec_cmd "sudo docker run -d $TASK_CONTAINER_OPTIONS $TASK_CONTAINER_IMAGE $TASK_CONTAINER_COMMAND"
+  ret=$?
+  trap before_exit EXIT
+  [ "$ret" != 0 ] && return $ret;
+
+  is_success=true
+}
+
+wait_for_exit() {
+  ret=0
+  is_success=false
+
+  exec_cmd "sudo docker wait $TASK_CONTAINER_NAME"
   ret=$?
   trap before_exit EXIT
   [ "$ret" != 0 ] && return $ret;
@@ -20,3 +32,6 @@ boot() {
 
 trap before_exit EXIT
 exec_grp "boot" "Booting up container for task: $TASK_NAME" "true"
+
+trap before_exit EXIT
+exec_grp "wait_for_exit" "Waiting for container:$TASK_CONTAINER_NAME to exit" "false"
